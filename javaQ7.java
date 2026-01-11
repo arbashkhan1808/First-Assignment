60c5490e83c67812ae67ae795797f064f47b7156
arbashkhan1808 <arbashkhan1808@gmail.com>
2026-01-11 13:21:20 +0500

    Add files via upload

---
+++ javaQ7.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+ public class javaQ7 {
+
+ 
+    public static void main(String[] args)
+     {
+        Scanner sc = new Scanner(System.in);
+      System.out.println("Enter an integer number: ");
+      int number = sc.nextInt();
+      if (number % 2 == 0) {
+        System.out.println("EVEN");
+      } else {
+        System.out.println("ODD");
+
+     }
+     sc.close();
+     }
+ }
